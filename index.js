add128e88493898d56946e511cc610b91a06c815
Pawan Thathsara <pawanthathsara987@gmail.com>
2025-07-30 23:58:21 +0530

    set api with api key and data checked

--- index.js
+++ index.js
@@ -1,15 +1,38 @@
 import express  from "express";
 import axios from "axios";
+import bodyParser from "body-parser";
+
 
 const app = express();
 const port = 3000;
+const API_KEY = "8f9fe522d8896b621e44b29672f83af7";
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", (req, res)=> {
     const hour = new Date().getHours();
     const isMorning = hour >= 6 && hour < 18;
     res.render("index.ejs", { isMorning });
+});
+
+app.post("/search", async (req, res) => {
+    let city = req.body.city;
+    
+    try{
+        const locationResponse = await axios.get("http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=5&appid=" + API_KEY);
+        const locationData = locationResponse.data;
+        const lat = locationData[0].lat;
+        const lon = locationData[0].lon;
+        
+        const weatherResponse = await axios.get("https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=" + API_KEY);
+        const weatherData = weatherResponse.data;
+        
+    }catch(error){
+        res.send(error);
+        console.log(error);
+    }
+
 });
 
 app.listen(port, ()=> {
