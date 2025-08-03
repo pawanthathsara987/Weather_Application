9b189f096f93d475f4ba9f8e7d824fe383c5dd63
Pawan Thathsara <pawanthathsara987@gmail.com>
2025-08-03 13:28:33 +0530

    update image changing times

--- public/js/getTime.js
+++ public/js/getTime.js
@@ -1,11 +1,17 @@
 const hour = new Date().getHours();
 
-if (hour > 6 && hour < 18) {
+if (hour > 6 && hour <= 12) {
     document.body.style.backgroundImage = "url('/images/morning1.jpg')";
     document.body.style.backgroundSize = "cover";
     document.body.style.backgroundRepeat = "no-repeat";
-} else {
-    document.body.style.backgroundImage = "url('/images/night1.jpg')";
+}else if(hour > 12 && hour <= 18 ){
+    document.body.style.backgroundImage = "url('/images/afternoon1.jpg')";
+    document.body.style.backgroundSize = "cover";
+    document.body.style.backgroundRepeat = "no-repeat";
+}else {
+    const nightImages = ["/images/night1.jpg", "/images/night2.jpg", "/images/night3.jpg"];
+    constrandomImage = Math.floor(Math.random() * nightImages.length);
+    document.body.style.backgroundImage = "url('" + nightImages[randomImage] + "')";
     document.body.style.backgroundSize = "cover";
     document.body.style.backgroundRepeat = "no-repeat";
 }
