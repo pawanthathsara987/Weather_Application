291fa23a97976fc23daba67a024e6736f1f180c8
Pawan Thathsara <pawanthathsara987@gmail.com>
2025-08-05 21:54:43 +0530

    updated background images order

--- public/js/getTime.js
+++ public/js/getTime.js
@@ -1,17 +1,20 @@
 const hour = new Date().getHours();
+let image = "";
 
 if (hour > 6 && hour <= 12) {
-    document.body.style.backgroundImage = "url('/images/morning1.jpg')";
-    document.body.style.backgroundSize = "cover";
-    document.body.style.backgroundRepeat = "no-repeat";
-}else if(hour > 12 && hour <= 18 ){
-    document.body.style.backgroundImage = "url('/images/afternoon1.jpg')";
-    document.body.style.backgroundSize = "cover";
-    document.body.style.backgroundRepeat = "no-repeat";
-}else {
-    const nightImages = ["/images/night1.jpg", "/images/night2.jpg", "/images/night3.jpg"];
-    constrandomImage = Math.floor(Math.random() * nightImages.length);
-    document.body.style.backgroundImage = "url('" + nightImages[randomImage] + "')";
-    document.body.style.backgroundSize = "cover";
-    document.body.style.backgroundRepeat = "no-repeat";
+    image = "/images/morning1.jpg";
+} else if (hour > 12 && hour <= 18) {
+    image = "/images/afternoon1.jpg";
+} else {
+    const nightImages = [
+        "/images/night1.jpg",
+        "/images/night2.jpg",
+        "/images/night3.jpg"
+    ];
+    const randomImage = Math.floor(Math.random() * nightImages.length);
+    image = nightImages[randomImage];
 }
+
+document.body.style.backgroundImage = `url('${image}')`;
+document.body.style.backgroundSize = "cover";
+document.body.style.backgroundRepeat = "no-repeat";
